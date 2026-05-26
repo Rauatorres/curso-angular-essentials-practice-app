f53d237409fdc3867eb9f9c5e64bb8243b38d8d6
rauatorres <rauatorres@gmail.com>
2026-05-26 13:01:03 -0300

    primeiros componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
+  imports: [Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('essentials-practice');
---
+++ src/app/calculate-form/calculate-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate-form',
+  imports: [],
+  templateUrl: './calculate-form.html',
+  styleUrl: './calculate-form.css',
+})
+export class CalculateForm {
+
+}
